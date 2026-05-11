92b920654ec26728afcc280ae3d3233ed1fd7ae5
German Yusufov <GERMAN.YUSUFOV05@bcmail.cuny.edu>
2026-05-10 22:08:39 -0400

    Add external job dataset integration

---
+++ app/discovery/jobs-data.ts
@@ -0,0 +1,23 @@
+export const jobs = [
+  {
+    id: 1,
+    title: "Senior React Developer",
+    company: "TechCorp",
+    location: "New York, NY",
+    posted: "4/30/2024",
+  },
+  {
+    id: 2,
+    title: "Full Stack Engineer",
+    company: "StartupXYZ",
+    location: "Remote",
+    posted: "5/9/2024",
+  },
+  {
+    id: 3,
+    title: "Frontend Developer",
+    company: "DesignStudio",
+    location: "Brooklyn, NY",
+    posted: "5/14/2024",
+  },
+];
